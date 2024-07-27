b5bc8179158be15d9bf4119713578edcfda88c0a
w-felipe360 <w.felipebraz@gmail.com>
2024-07-27 19:07:21 -0300

    Add CORS configuration for backend API

---
+++ backend/src/main/java/com/fibboproject/backend/config/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.fibboproject.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/buslines/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+
+}
